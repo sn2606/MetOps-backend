493625c43929d2501d42ae6ee3e6f644781b1488
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-09 15:54:38 +0530

    models & migrations

---
+++ query/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+User = get_user_model()
+
+
+class MetQuery(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4)
+    location = models.CharField(max_length=100, blank=True)
+
+
+class QueryForUser(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(
+        User, related_name='user_id', on_delete=models.CASCADE)
+    query_id = models.ForeignKey(
+        MetQuery, related_name='query_id_main', on_delete=models.CASCADE)
--- records/models.py
+++ records/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from query.models import MetQuery
 
 # Create your models here.
+
+
+class Record(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    height = models.IntegerField()
+    actual_temperature = models.DecimalField(max_digits=10, decimal_places=4)
+    virtual_temperature = models.DecimalField(max_digits=10, decimal_places=4)
+    pressure = models.DecimalField(max_digits=10, decimal_places=4)
+    humidity = models.DecimalField(max_digits=10, decimal_places=4)
+    wind_speed = models.DecimalField(max_digits=10, decimal_places=4)
+    wind_direction = models.DecimalField(max_digits=10, decimal_places=4)
+
+
+class RecordForQuery(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    query_id = models.ForeignKey(
+        MetQuery, related_name='query_id', on_delete=models.CASCADE)
+    record_id = models.ForeignKey(
+        Record, related_name='record_id', on_delete=models.CASCADE)
