0bb018b0562520e1c29c46d5e9ca1b6c0059cc49
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-14 18:52:42 +0530

    urls reconfig

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('', views.api_home)
+    path('', views.api_home),
+    path('records/', include('records.urls')),
+    path('query/', include('query.urls')),
+    path('users/', include('users.urls')),
 ]
--- metops/urls.py
+++ metops/urls.py
@@ -4,6 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/records/', include('records.urls')),
-    path('api/query/', include('query.urls')),
 ]
